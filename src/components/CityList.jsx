67caafe42453129a8adf801f646197f50ab07779
RANUJ CHAUDHARY <tridevcomputer01@gmail.com>
2024-02-20 20:35:02 +0530

    learned about useNavigate useSearchParams Navlinks Link

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,15 +2,18 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { Link } from "react-router-dom";
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
   if(!cities.length) return <Message message={"Add your first city by clicking on a city on the map."}/>
   return (
+  
     <ul className={styles.cityList}>
       {cities.map((city) => (
         <CityItem city={city} key={city.id} />
       ))}
     </ul>
+
   );
 }
 
